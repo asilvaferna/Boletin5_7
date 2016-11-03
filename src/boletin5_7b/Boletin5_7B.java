84ebf95dd71d4cd6de2834862d8bc6c4d8a2778a
Adrián Silva <adri4silva@gmail.com>
2016-11-03 09:39:10 +0100

    Area triangulo

--- src/boletin5_7b/Boletin5_7B.java
+++ src/boletin5_7b/Boletin5_7B.java
@@ -5,6 +5,7 @@ public class Boletin5_7B {
         Triangulo t1 = new Triangulo();
         Circulo cr1 = new Circulo();
         Cuadrado cd1 =  new Cuadrado();
+        t1.areaTriangulo(3, 4);
         
         
     }
