53cb0bffc0cee7b4cbc6a75b06d0fb7f9311f686
Adrián Silva <adri4silva@gmail.com>
2016-11-03 09:37:39 +0100

    Creados objetos

--- src/boletin5_7b/Boletin5_7B.java
+++ src/boletin5_7b/Boletin5_7B.java
@@ -2,7 +2,10 @@ package boletin5_7b;
 public class Boletin5_7B {
     public static void main(String[] args) {
 
-       
+        Triangulo t1 = new Triangulo();
+        Circulo cr1 = new Circulo();
+        Cuadrado cd1 =  new Cuadrado();
+        
         
     }
     
